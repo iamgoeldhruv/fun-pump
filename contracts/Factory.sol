6db06738e8bc0cf59493f5035d874cd37a2a0bd0
Dhruv <goeldhruv9876@gmail.com>
2025-08-15 17:10:01 +0530

    Added all test for tokan and and started implementing logic for token sale

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -6,12 +6,22 @@ contract Factory {
    uint256 public  immutable fee;
    address public owner;
    address[] public tokens;
+   struct TokenSale{
+      address token;
+      string name;
+      address creator;
+      uint256 sold;
+      uint256 raised;
+      bool isOpen;
+
+
+   }
    uint256 public totakTokens=0;
       constructor(uint256 _fee){
     fee=_fee;
     owner=msg.sender;
    }
-   function create(string memory _name,string memory _symbol){
+   function create(string memory _name,string memory _symbol) external payable{
       Token token=new Token(msg.sender,_name,_symbol,1_000_000 ether);
       tokens.push(address(token));
       totakTokens++;
--- test/Factory.js
+++ test/Factory.js
@@ -1,35 +1,55 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Factory", function () {
-    const FEE = ethers.parseUnits("0.01", 18); // BigInt in Ethers v6
+  const FEE = ethers.parseUnits("0.01", 18); // BigInt in Ethers v6
 
-    async function deployFactoryFixture() {
-        const [deployer,creator]=await ethers.getSigners();
-        const Factory = await ethers.getContractFactory("Factory");
-        const factory = await Factory.deploy(FEE);
-        const transaction=await factory.connect(creator).create("TestToken", "TTK",{value:FEE});
-        await transaction.wait();
-        return { factory,deployer,creator};
-    }
+  async function deployFactoryFixture() {
+    const [deployer, creator] = await ethers.getSigners();
+    const Factory = await ethers.getContractFactory("Factory");
+    const factory = await Factory.deploy(FEE);
+    const transaction = await factory
+      .connect(creator)
+      .create("TestToken", "TTK", { value: FEE });
+    await transaction.wait();
+    const tokenAddress = await factory.tokens(0);
+    const token = await ethers.getContractAt("Token", tokenAddress);
+    return { factory, token, deployer, creator };
+  }
 
-    describe("Deployment", function () {
-        it("Should set the right fee", async function () {
-            const { factory } = await loadFixture(deployFactoryFixture);
-            expect(await factory.fee()).to.equal(FEE);
-        });
-         it("Should set the owner", async function () {
-            const { factory,deployer } = await loadFixture(deployFactoryFixture);
-            expect(await factory.owner()).to.equal(deployer.address);
-        });
+  describe("Deployment", function () {
+    it("Should set the right fee", async function () {
+      const { factory } = await loadFixture(deployFactoryFixture);
+      expect(await factory.fee()).to.equal(FEE);
     });
-    describe("creating",function(){
-        it("should set the owner",async function(){
+    it("Should set the owner", async function () {
+      const { factory, deployer } = await loadFixture(deployFactoryFixture);
+      expect(await factory.owner()).to.equal(deployer.address);
+    });
+  });
+  describe("creating", function () {
+    it("should set the owner", async function () {
+      const { factory, token } = await loadFixture(deployFactoryFixture);
+      expect(await token.owner()).to.equal(await factory.getAddress());
+    });
+    it("should set the creator", async function () {
+      const { token, creator } = await loadFixture(deployFactoryFixture);
+      expect(await token.creator()).to.equal(creator.address);
+    });
+    it("should set the supply", async function () {
+      const { token} = await loadFixture(deployFactoryFixture);
+      const totalSupply = await ethers.parseUnits("1000000", 18);
+      expect(await token.totalSupply()).to.equal(totalSupply);
+    });
+     it("should update eth balance", async function () {
+      const {factory, token} = await loadFixture(deployFactoryFixture);
+      const balance = await ethers.provider.getBalance(await factory.getAddress());
+        expect(balance).to.equal(FEE);
 
-        } 
-            
-        )
-
-    })
+     
+    });
+  });
 });
